9907a9339172bfcc158d5e2970d500fe18504c20
DylanAPDavis <dylan.ap.davis@gmail.com>
2016-06-14 20:59:30 -0400

    Add VLAN pruning to pruning service, update TopPCE to use new pruning method call.

--- core/src/main/java/net/es/oscars/pce/PruningService.java
+++ core/src/main/java/net/es/oscars/pce/PruningService.java
@@ -1,14 +1,20 @@
 package net.es.oscars.pce;
 
 import lombok.extern.slf4j.Slf4j;
+import net.es.oscars.resv.ent.RequestedVlanFlowE;
+import net.es.oscars.resv.ent.RequestedVlanJunctionE;
+import net.es.oscars.resv.ent.RequestedVlanPipeE;
 import net.es.oscars.topo.beans.TopoEdge;
 import net.es.oscars.topo.beans.Topology;
+import net.es.oscars.topo.ent.IntRangeE;
 import net.es.oscars.topo.ent.ReservableBandwidthE;
+import net.es.oscars.topo.ent.ReservableVlanE;
 import net.es.oscars.topo.svc.TopoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -21,15 +27,57 @@ public class PruningService {
     @Autowired
     private TopoService topoService;
 
-    public Topology pruneTopology(Topology topo, Integer azBw, Integer zaBw){
+    public Topology pruneForPipe(Topology topo, RequestedVlanPipeE pipe){
+        Integer azBw = pipe.getAzMbps();
+        Integer zaBw = pipe.getZaMbps();
+        Set<String> vlans = new HashSet<>();
+        vlans.addAll(getVlansFromJunction(pipe.getAJunction()));
+        vlans.addAll(getVlansFromJunction(pipe.getZJunction()));
+        return pruneTopology(topo, azBw, zaBw, vlans);
+    }
+
+    private Set<String> getVlansFromJunction(RequestedVlanJunctionE junction){
+        return junction.getFixtures().stream().map(j -> j.getVlanExpression()).collect(Collectors.toSet());
+    }
+
+    public Topology pruneTopology(Topology topo, Integer azBw, Integer zaBw, Set<String> vlans){
         Topology pruned = new Topology();
         pruned.setLayer(topo.getLayer());
         pruned.setVertices(topo.getVertices());
         List<ReservableBandwidthE> bandwidths = topoService.reservableBandwidths();
-        Set<TopoEdge> availableEdges = topo.getEdges().stream().filter(e -> availableBW(e, azBw, zaBw, bandwidths))
+        List<ReservableVlanE> resvVlans = topoService.reservableVlans();
+        Set<TopoEdge> availableEdges = topo.getEdges().stream()
+                .filter(e -> availableBW(e, azBw, zaBw, bandwidths))
+                .filter(e -> availableVlans(e, vlans, resvVlans))
                 .collect(Collectors.toSet());
         pruned.setEdges(availableEdges);
         return pruned;
+    }
+
+    private boolean availableVlans(TopoEdge edge, Set<String> vlans, List<ReservableVlanE> resvVlans) {
+        String aUrn = edge.getA().getUrn();
+        String zUrn = edge.getZ().getUrn();
+        List<ReservableVlanE> aMatching = resvVlans.stream().filter(v -> v.getUrn().getUrn().equals(aUrn))
+                .collect(Collectors.toList());
+        List<ReservableVlanE> zMatching = resvVlans.stream().filter(v -> v.getUrn().getUrn().equals(zUrn))
+                .collect(Collectors.toList());
+
+        assert aMatching.size() <= 1 && zMatching.size() <= 1;
+        if(aMatching.isEmpty() || zMatching.isEmpty()){
+            return true;
+        } else{
+            for(String vlanString : vlans){
+                Integer vlanInt = Integer.parseInt(vlanString);
+                boolean aContainsVlan = aMatching.get(0).getVlanRanges().stream().map(IntRangeE::toDtoIntRange)
+                        .anyMatch(vr -> vr.contains(vlanInt));
+                boolean zContainsVlan = zMatching.get(0).getVlanRanges().stream().map(IntRangeE::toDtoIntRange)
+                        .anyMatch(vr -> vr.contains(vlanInt));
+                if(!aContainsVlan || !zContainsVlan){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
     private boolean availableBW(TopoEdge edge, Integer azBw, Integer zaBw, List<ReservableBandwidthE> bandwidths){
