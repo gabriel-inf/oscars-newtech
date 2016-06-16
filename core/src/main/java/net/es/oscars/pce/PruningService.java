c0cad08708020bf46fbdb517c6253eaeffaf72a1
DylanAPDavis <dylan.ap.davis@gmail.com>
2016-06-16 12:49:20 -0400

    Add assert statements to pruneForPipe() method, add building blocks for finding any available VLAN.

--- core/src/main/java/net/es/oscars/pce/PruningService.java
+++ core/src/main/java/net/es/oscars/pce/PruningService.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -66,6 +67,10 @@ public class PruningService {
     }
 
     public Topology pruneForPipe(Topology topo, RequestedVlanPipeE pipe){
+        assert(pipe != null);
+        assert(topo != null);
+        assert(urnRepo != null);
+        assert(urnRepo.findAll() != null);
         return pruneForPipe(topo, pipe, urnRepo.findAll());
     }
 
@@ -78,11 +83,37 @@ public class PruningService {
         return pruneTopology(topo, azBw, zaBw, vlans, urns);
     }
 
-    private Set<Integer> findOpenVlans(Set<TopoEdge> edges){
+    private Set<Integer> findOpenVlans(Set<TopoEdge> edges, List<UrnE> urns){
         //TODO: Figure out best way to find a VLAN that will work across all edges
         // Note: MPLS edges can use any available VLAN
 
+        TopoEdge e = edges.iterator().next();
+        Set<IntRange> aRanges = getVlanRangesFromUrn(urns, e.getA().getUrn());
+        Set<IntRange> zRanges = getVlanRangesFromUrn(urns, e.getZ().getUrn());
+        Set<Integer> overlap = new HashSet<>();
+        overlap = addToOverlap(overlap, aRanges);
+        overlap = addToOverlap(overlap, zRanges);
         return new HashSet<Integer>();
+    }
+
+    private Set<Integer> addToOverlap(Set<Integer> overlap, Set<IntRange> ranges){
+        for(IntRange range : ranges){
+            Set<Integer> numbersInRange = getSetOfNumbersInRange(range);
+            if(overlap.isEmpty()){
+                overlap.addAll(numbersInRange);
+            }else{
+                overlap.retainAll(numbersInRange);
+            }
+        }
+        return overlap;
+    }
+
+    private Set<Integer> getSetOfNumbersInRange(IntRange aRange) {
+        Set<Integer> numbers = new HashSet<>();
+        for(Integer num = aRange.getFloor(); num <= aRange.getCeiling(); num++){
+            numbers.add(num);
+        }
+        return numbers;
     }
 
     private Set<Integer> getVlansFromJunction(RequestedVlanJunctionE junction){
@@ -98,40 +129,35 @@ public class PruningService {
                 .filter(e -> availableBW(e, azBw, zaBw, urns))
                 .collect(Collectors.toSet());
         if(vlans == null){
-            Set<Integer> openVlans = findOpenVlans(availableEdges);
+            Set<Integer> openVlans = findOpenVlans(availableEdges, urns);
         }
         pruned.setEdges(availableEdges);
         return pruned;
     }
 
-    private boolean availableVlans(TopoEdge edge, Set<Integer> vlans, List<UrnE> urns) {
-        List<UrnE> aMatching = urns.stream()
-                .filter(u -> u.getUrn().equals(edge.getA().getUrn()))
-                .collect(Collectors.toList());
-        List<UrnE> zMatching = urns.stream()
-                .filter(u -> u.getUrn().equals(edge.getZ().getUrn()))
-                .collect(Collectors.toList());
+    private Set<IntRange> getVlanRangesFromUrn(List<UrnE> urns, String matchingUrn){
+        return urns.stream()
+                .filter(u -> u.getUrn().equals(matchingUrn))
+                .map(UrnE::getReservableVlans)
+                .map(ReservableVlanE::getVlanRanges)
+                .flatMap(Collection::stream)
+                .map(IntRangeE::toDtoIntRange)
+                .collect(Collectors.toSet());
+    }
 
-        assert aMatching.size() <= 1 && zMatching.size() <= 1;
-        if(aMatching.isEmpty() || zMatching.isEmpty()){
+
+    private boolean availableVlans(TopoEdge edge, Set<Integer> vlans, List<UrnE> urns) {
+        Set<IntRange> aRanges = getVlanRangesFromUrn(urns, edge.getA().getUrn());
+        Set<IntRange> zRanges = getVlanRangesFromUrn(urns, edge.getZ().getUrn());
+
+        assert aRanges.size() <= 1 && zRanges.size() <= 1;
+        if(aRanges.isEmpty() || zRanges.isEmpty()){
             return true;
         } else{
-            Stream<IntRange> aVlanRangeStream = aMatching.get(0)
-                    .getReservableVlans()
-                    .getVlanRanges()
-                    .stream()
-                    .map(IntRangeE::toDtoIntRange);
-            Stream<IntRange> zVlanRangeStream = aMatching.get(0)
-                    .getReservableVlans()
-                    .getVlanRanges()
-                    .stream()
-                    .map(IntRangeE::toDtoIntRange);
 
             //Edge is in MPLS
-            //This implies that any one VLAN tag (available on both ends of the edge) can work
+            //Any one VLAN tag (available on both ends of the edge) can work
             if(edge.getLayer().equals(Layer.MPLS)){
-                Set<IntRange> aRanges = aVlanRangeStream.collect(Collectors.toSet());
-                Set<IntRange> zRanges = zVlanRangeStream.collect(Collectors.toSet());
                 for(IntRange aRange : aRanges){
                     for(IntRange zRange: zRanges){
                         if(checkVlanRangeOverlap(aRange, zRange)){
@@ -142,8 +168,8 @@ public class PruningService {
                 return false;
             }
             for(Integer requestedVlan : vlans){
-                boolean aContainsVlan = aVlanRangeStream.anyMatch(vr -> vr.contains(requestedVlan));
-                boolean zContainsVlan = zVlanRangeStream.anyMatch(vr -> vr.contains(requestedVlan));
+                boolean aContainsVlan = aRanges.stream().anyMatch(vr -> vr.contains(requestedVlan));
+                boolean zContainsVlan = zRanges.stream().anyMatch(vr -> vr.contains(requestedVlan));
                 if(!aContainsVlan || !zContainsVlan){
                     return false;
                 }
