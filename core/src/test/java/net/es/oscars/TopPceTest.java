b2daac9ed307571ef32092017dbc40295328c6cc
JeremyPlante <Jeremy_Plante@student.uml.edu>
2016-07-06 18:53:18 -0400

    Third PCE Test added: Fails.

--- core/src/test/java/net/es/oscars/TopPceTest.java
+++ core/src/test/java/net/es/oscars/TopPceTest.java
@@ -173,7 +173,7 @@ public class TopPceTest
                     allResBWs.add(j.getFixtures().iterator().next().getReservedBandwidth());
                     allResBWs.add(j.getFixtures().iterator().next().getReservedBandwidth());
                 });
-        log.info("HERE 2");
+
         allResBWs.stream()
                 .forEach(bw -> {
                     assert(bw.getInBandwidth() == bw.getEgBandwidth());
@@ -181,6 +181,99 @@ public class TopPceTest
                 });
 
         log.info("test 'basicPceTest2' passed.");
+    }
+
+    @Test
+    public void basicPceTest3()
+    {
+        log.info("Initializing test: 'basicPceTest3'.");
+
+        RequestedBlueprintE requestedBlueprint;
+        ReservedBlueprintE reservedBlueprint = null;
+        ScheduleSpecificationE requestedSched;
+
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
+
+        String srcPort = "portA";
+        String srcDevice = "nodeK";
+        String dstPort = "portZ";
+        String dstDevice = "nodeQ";
+        Integer azBW = 25;
+        Integer zaBW = 25;
+        Boolean palindrome = true;
+        String vlan = "any";
+
+        this.buildTopo3();
+        requestedSched = testBuilder.buildSchedule(startDate, endDate);
+        requestedBlueprint = testBuilder.buildRequest(srcPort, srcDevice, dstPort, dstDevice, azBW, zaBW, palindrome, vlan);
+
+        log.info("Beginning test: 'basicPceTest3'.");
+
+        try
+        {
+            reservedBlueprint = topPCE.makeReserved(requestedBlueprint, requestedSched);
+        }
+        catch(PCEException | PSSException pceE){ log.error("", pceE); }
+
+        assert(reservedBlueprint != null);
+
+        ReservedVlanFlowE reservedFlow = reservedBlueprint.getVlanFlows().iterator().next();
+
+        Set<ReservedEthPipeE> allResPipes = reservedFlow.getPipes();
+        Set<ReservedVlanJunctionE> allResJunctions = reservedFlow.getJunctions();
+        List<ReservedBandwidthE> allResBWs = new ArrayList<>();
+
+        assert(allResPipes.size() == 1);
+        assert(allResJunctions.size() == 0);
+
+        ReservedEthPipeE onlyPipe = allResPipes.iterator().next();
+
+        ReservedVlanJunctionE juncA = onlyPipe.getAJunction();
+        ReservedVlanJunctionE juncZ = onlyPipe.getZJunction();
+        Iterator<ReservedVlanFixtureE> iterA = juncA.getFixtures().iterator();
+        Iterator<ReservedVlanFixtureE> iterZ = juncZ.getFixtures().iterator();
+
+        ReservedVlanFixtureE fixA1 = iterA.next();
+        ReservedVlanFixtureE fixA2 = iterA.next();
+        ReservedVlanFixtureE fixZ1 = iterZ.next();
+        ReservedVlanFixtureE fixZ2 = iterZ.next();
+
+        assert(juncA.getDeviceUrn().getUrn().equals("nodeK"));
+        assert(juncZ.getDeviceUrn().getUrn().equals("nodeQ"));
+        assert(fixA1.getIfceUrn().getUrn().equals("portA") || fixA1.getIfceUrn().getUrn().equals("nodeK:1"));
+        assert(fixA2.getIfceUrn().getUrn().equals("portA") || fixA2.getIfceUrn().getUrn().equals("nodeK:1"));
+        assert(fixZ1.getIfceUrn().getUrn().equals("portA") || fixZ1.getIfceUrn().getUrn().equals("nodeK:1"));
+        assert(fixZ2.getIfceUrn().getUrn().equals("portA") || fixZ2.getIfceUrn().getUrn().equals("nodeK:1"));
+
+        assert(fixA1.getReservedBandwidth().getInBandwidth() == azBW);
+        assert(fixA2.getReservedBandwidth().getInBandwidth() == azBW);
+        assert(fixZ1.getReservedBandwidth().getInBandwidth() == azBW);
+        assert(fixZ2.getReservedBandwidth().getInBandwidth() == azBW);
+        assert(fixA1.getReservedBandwidth().getEgBandwidth() == azBW);
+        assert(fixA2.getReservedBandwidth().getEgBandwidth() == azBW);
+        assert(fixZ1.getReservedBandwidth().getEgBandwidth() == azBW);
+        assert(fixZ2.getReservedBandwidth().getEgBandwidth() == azBW);
+
+        List<String> azERO = onlyPipe.getAzERO();
+        List<String> zaERO = onlyPipe.getZaERO();
+
+        String actualAzERO = "";
+        String actualZaERO = "";
+
+        for(String x : azERO)
+            actualAzERO = actualAzERO + x + "-";
+
+        for(String x : zaERO)
+            actualZaERO = actualZaERO + x + "-";
+
+        String expectedAzERO = "nodeK-nodeK:1-nodeP:1-nodeP-nodeP:2-nodeQ:1-nodeQ-";
+        String expectedZaERO = "nodeQ-nodeQ:1-nodeP:2-nodeP-nodeP:1-nodeK:1-nodeK-";
+
+        assert(actualAzERO.equals(expectedAzERO));
+        assert(actualZaERO.equals(expectedZaERO));
+        
+        log.info("test 'basicPceTest3' passed.");
     }
 
     private void buildSimpleTopo()
@@ -332,6 +425,132 @@ public class TopPceTest
         topoLinks.add(edgeEth_N1_L3);
         topoLinks.add(edgeEth_N2_M2);
         topoLinks.add(edgeEth_P1_L1);
+
+        // Map Ports to Devices for simplicity in utility class //
+        for(TopoEdge oneEdge : topoLinks)
+        {
+            if(oneEdge.getLayer().equals(Layer.INTERNAL))
+            {
+                if(oneEdge.getA().getVertexType().equals(VertexType.PORT))
+                {
+                    portDeviceMap.put(oneEdge.getA(), oneEdge.getZ());
+                }
+            }
+        }
+
+        testBuilder.populateRepos(topoNodes, topoLinks, portDeviceMap);
+    }
+
+    private void buildTopo3()
+    {
+        log.info("Building Test Topology 3");
+
+        Map<TopoVertex, TopoVertex> portDeviceMap = new HashMap<>();
+
+        // Devices //
+        TopoVertex nodeK = new TopoVertex("nodeK", VertexType.SWITCH);
+        TopoVertex nodeP = new TopoVertex("nodeP", VertexType.ROUTER);
+        TopoVertex nodeQ = new TopoVertex("nodeQ", VertexType.ROUTER);
+        TopoVertex nodeR = new TopoVertex("nodeR", VertexType.ROUTER);
+
+        // Ports //
+        TopoVertex portA = new TopoVertex("portA", VertexType.PORT);
+        TopoVertex portZ = new TopoVertex("portZ", VertexType.PORT);
+        TopoVertex portK1 = new TopoVertex("nodeK:1", VertexType.PORT);
+        TopoVertex portP1 = new TopoVertex("nodeP:1", VertexType.PORT);
+        TopoVertex portP2 = new TopoVertex("nodeP:2", VertexType.PORT);
+        TopoVertex portP3 = new TopoVertex("nodeP:3", VertexType.PORT);
+        TopoVertex portQ1 = new TopoVertex("nodeQ:1", VertexType.PORT);
+        TopoVertex portQ2 = new TopoVertex("nodeQ:2", VertexType.PORT);
+        TopoVertex portR1 = new TopoVertex("nodeR:1", VertexType.PORT);
+        TopoVertex portR2 = new TopoVertex("nodeR:2", VertexType.PORT);
+
+        // End-Port Links //
+        TopoEdge edgeInt_A_K = new TopoEdge(portA, nodeK, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_Z_Q = new TopoEdge(portZ, nodeQ, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_K_A = new TopoEdge(nodeK, portA, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_Q_Z = new TopoEdge(nodeQ, portZ, 0L, Layer.INTERNAL);
+
+        // Internal Links //
+        TopoEdge edgeInt_K1_K = new TopoEdge(portK1, nodeK, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_P1_P = new TopoEdge(portP1, nodeP, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_P2_P = new TopoEdge(portP2, nodeP, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_P3_P = new TopoEdge(portP3, nodeP, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_Q1_Q = new TopoEdge(portQ1, nodeQ, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_Q2_Q = new TopoEdge(portQ2, nodeQ, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_R1_R = new TopoEdge(portR1, nodeR, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_R2_R = new TopoEdge(portR2, nodeR, 0L, Layer.INTERNAL);
+
+        // Internal-Reverse Links //
+        TopoEdge edgeInt_K_K1 = new TopoEdge(nodeK, portK1, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_P_P1 = new TopoEdge(nodeP, portP1, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_P_P2 = new TopoEdge(nodeP, portP2, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_P_P3 = new TopoEdge(nodeP, portP3, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_Q_Q1 = new TopoEdge(nodeQ, portQ1, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_Q_Q2 = new TopoEdge(nodeQ, portQ2, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_R_R1 = new TopoEdge(nodeR, portR1, 0L, Layer.INTERNAL);
+        TopoEdge edgeInt_R_R2 = new TopoEdge(nodeR, portR2, 0L, Layer.INTERNAL);
+
+        // Network Links //
+        TopoEdge edgeEth_K1_P1 = new TopoEdge(portK1, portP1, 100L, Layer.ETHERNET);
+        TopoEdge edgeEth_P1_K1 = new TopoEdge(portP1, portK1, 100L, Layer.ETHERNET);
+        TopoEdge edgeMpls_P2_Q1 = new TopoEdge(portP2, portQ1, 100L, Layer.MPLS);
+        TopoEdge edgeMpls_P3_R1 = new TopoEdge(portP3, portR1, 100L, Layer.MPLS);
+        TopoEdge edgeMpls_Q1_P2 = new TopoEdge(portQ1, portP2, 100L, Layer.MPLS);
+        TopoEdge edgeMpls_Q2_R2 = new TopoEdge(portQ2, portR2, 100L, Layer.MPLS);
+        TopoEdge edgeMpls_R1_P3 = new TopoEdge(portR1, portP3, 100L, Layer.MPLS);
+        TopoEdge edgeMpls_R2_Q2 = new TopoEdge(portR2, portQ2, 100L, Layer.MPLS);
+
+
+        List<TopoVertex> topoNodes = new ArrayList<>();
+        List<TopoEdge> topoLinks = new ArrayList<>();
+
+        topoNodes.add(nodeK);
+        topoNodes.add(nodeP);
+        topoNodes.add(nodeQ);
+        topoNodes.add(nodeR);
+
+        topoNodes.add(portA);
+        topoNodes.add(portZ);
+        topoNodes.add(portK1);
+        topoNodes.add(portP1);
+        topoNodes.add(portP2);
+        topoNodes.add(portP3);
+        topoNodes.add(portQ1);
+        topoNodes.add(portQ2);
+        topoNodes.add(portR1);
+        topoNodes.add(portR2);
+
+        topoLinks.add(edgeInt_A_K);
+        topoLinks.add(edgeInt_Z_Q);
+        topoLinks.add(edgeInt_K1_K);
+        topoLinks.add(edgeInt_P1_P);
+        topoLinks.add(edgeInt_P2_P);
+        topoLinks.add(edgeInt_P3_P);
+        topoLinks.add(edgeInt_Q1_Q);
+        topoLinks.add(edgeInt_Q2_Q);
+        topoLinks.add(edgeInt_R1_R);
+        topoLinks.add(edgeInt_R2_R);
+
+        topoLinks.add(edgeInt_K_A);
+        topoLinks.add(edgeInt_Q_Z);
+        topoLinks.add(edgeInt_K_K1);
+        topoLinks.add(edgeInt_P_P1);
+        topoLinks.add(edgeInt_P_P2);
+        topoLinks.add(edgeInt_P_P3);
+        topoLinks.add(edgeInt_Q_Q1);
+        topoLinks.add(edgeInt_Q_Q2);
+        topoLinks.add(edgeInt_R_R1);
+        topoLinks.add(edgeInt_R_R2);
+
+        topoLinks.add(edgeEth_K1_P1);
+        topoLinks.add(edgeEth_P1_K1);
+        topoLinks.add(edgeMpls_P2_Q1);
+        topoLinks.add(edgeMpls_P3_R1);
+        topoLinks.add(edgeMpls_Q1_P2);
+        topoLinks.add(edgeMpls_Q2_R2);
+        topoLinks.add(edgeMpls_R1_P3);
+        topoLinks.add(edgeMpls_R2_Q2);
 
         // Map Ports to Devices for simplicity in utility class //
         for(TopoEdge oneEdge : topoLinks)
