c26002537438a3a8d1b4c5c86a878660c54f0c50
DylanAPDavis <dylan.ap.davis@gmail.com>
2016-08-15 19:15:17 -0400

    Test blacklist pruning through PCE stack.

--- core/src/test/java/net/es/oscars/pce/BlacklistPruningTest.java
+++ core/src/test/java/net/es/oscars/pce/BlacklistPruningTest.java
@@ -21,10 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -62,24 +59,199 @@ public class BlacklistPruningTest {
         blacklist.add("nodeR");
 
         // Pipe Setup
-        RequestedBlueprintE requestedBlueprint;
-        ScheduleSpecificationE requestedSched;
         Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
         Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
-        String srcPort = "portA";
-        String srcDevice = "nodeK";
-        String dstPort = "portZ";
-        String dstDevice = "nodeQ";
+        ScheduleSpecificationE requestedSched = testBuilder.buildSchedule(startDate, endDate);
+
+        RequestedBlueprintE requestedBlueprint =  setupTest("portA", "nodeK", "portZ", "nodeQ", blacklist);
+
+        log.info("Beginning test: 'blacklistMplsTest'.");
+        pruneTest(requestedBlueprint, topo, requestedSched, origEdges, origVerts, blacklist);
+        log.info("'blacklistMplsTest' passed.");
+    }
+
+    @Test
+    public void blacklistEthTest(){
+        log.info("Initializing test: 'blacklistEthTest'.");
+
+        topologyBuilder.buildTopo4();
+        Topology topo = topoService.getMultilayerTopology();
+        Set<TopoEdge> origEdges = new HashSet<>(topo.getEdges());
+        Set<TopoVertex> origVerts = new HashSet<>(topo.getVertices());
+
+        Set<String> blacklist = new HashSet<>();
+        blacklist.add("nodeL");
+        blacklist.add("nodeM");
+
+        // Pipe Setup
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
+        ScheduleSpecificationE requestedSched = testBuilder.buildSchedule(startDate, endDate);
+
+        RequestedBlueprintE requestedBlueprint =  setupTest("portA", "nodeK", "portZ", "nodeQ", blacklist);
+
+        log.info("Beginning test: 'blacklistEthTest'.");
+        pruneTest(requestedBlueprint, topo, requestedSched, origEdges, origVerts, blacklist);
+        log.info("'blacklistEthTest' passed.");
+    }
+
+    @Test
+    public void blacklistEthMplsTest(){
+        log.info("Initializing test: 'blacklistEthMplsTest'.");
+
+        topologyBuilder.buildTopo4();
+        Topology topo = topoService.getMultilayerTopology();
+        Set<TopoEdge> origEdges = new HashSet<>(topo.getEdges());
+        Set<TopoVertex> origVerts = new HashSet<>(topo.getVertices());
+
+        Set<String> blacklist = new HashSet<>();
+        blacklist.add("nodeL");
+        blacklist.add("nodeM");
+        blacklist.add("nodeP");
+        blacklist.add("nodeR");
+
+        // Pipe Setup
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
+        ScheduleSpecificationE requestedSched = testBuilder.buildSchedule(startDate, endDate);
+
+        RequestedBlueprintE requestedBlueprint =  setupTest("portA", "nodeK", "portZ", "nodeQ", blacklist);
+
+        log.info("Beginning test: 'blacklistEthMplsTest'.");
+        pruneTest(requestedBlueprint, topo, requestedSched, origEdges, origVerts, blacklist);
+        log.info("'blacklistEthMplsTest' passed.");
+    }
+
+    @Test
+    public void blacklistAllTest(){
+        log.info("Initializing test: 'blacklistAllTest'.");
+
+        topologyBuilder.buildTopo4();
+        Topology topo = topoService.getMultilayerTopology();
+        Set<TopoEdge> origEdges = new HashSet<>(topo.getEdges());
+        Set<TopoVertex> origVerts = new HashSet<>(topo.getVertices());
+
+        Set<String> blacklist = new HashSet<>();
+        blacklist.add("nodeK");
+        blacklist.add("nodeL");
+        blacklist.add("nodeM");
+        blacklist.add("nodeP");
+        blacklist.add("nodeQ");
+        blacklist.add("nodeR");
+
+        // Pipe Setup
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
+        ScheduleSpecificationE requestedSched = testBuilder.buildSchedule(startDate, endDate);
+
+        RequestedBlueprintE requestedBlueprint =  setupTest("portA", "nodeK", "portZ", "nodeQ", blacklist);
+
+        log.info("Beginning test: 'blacklistAllTest'.");
+        pruneTest(requestedBlueprint, topo, requestedSched, origEdges, origVerts, blacklist);
+        log.info("'blacklistAllTest' passed.");
+    }
+
+    @Test
+    public void pceSubmitBlacklistAllTest(){
+        log.info("Initializing test: 'pceSubmitBlacklistAllTest'.");
+
+        topologyBuilder.buildTopo4();
+        Topology topo = topoService.getMultilayerTopology();
+
+        Set<String> blacklist = new HashSet<>();
+        blacklist.add("nodeK");
+        blacklist.add("nodeL");
+        blacklist.add("nodeM");
+        blacklist.add("nodeP");
+        blacklist.add("nodeQ");
+        blacklist.add("nodeR");
+
+        // Pipe Setup
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
+        ScheduleSpecificationE requestedSched = testBuilder.buildSchedule(startDate, endDate);
+
+        RequestedBlueprintE requestedBlueprint =  setupTest("portA", "nodeK", "portZ", "nodeQ", blacklist);
+
+        log.info("Beginning test: 'pceSubmitBlacklistAllTest'.");
+        try{
+            Optional<ReservedBlueprintE> opt = topPCE.makeReserved(requestedBlueprint, requestedSched);
+            assert(!opt.isPresent());
+        } catch (Exception e){
+            log.error(e.toString());
+        }
+        log.info("'pceSubmitBlacklistAllTest' passed.");
+    }
+
+    @Test
+    public void pceSubmitBlacklistIntermediateTest(){
+        log.info("Initializing test: 'pceSubmitBlacklistAllTest'.");
+
+        topologyBuilder.buildTopo4();
+
+        Set<String> blacklist = new HashSet<>();
+        blacklist.add("nodeL");
+        blacklist.add("nodeP");
+
+        // Pipe Setup
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
+        ScheduleSpecificationE requestedSched = testBuilder.buildSchedule(startDate, endDate);
+        RequestedBlueprintE requestedBlueprint =  setupTest("portA", "nodeK", "portZ", "nodeQ", blacklist);
+
+        log.info("Beginning test: 'pceSubmitBlacklistAllTest'.");
+        ReservedBlueprintE resBlueprint = null;
+        try{
+            Optional<ReservedBlueprintE> opt = topPCE.makeReserved(requestedBlueprint, requestedSched);
+            assert(opt.isPresent());
+            resBlueprint = opt.get();
+        } catch (Exception e){
+            log.error(e.toString());
+        }
+
+        assert(resBlueprint != null);
+        Set<ReservedEthPipeE> ethPipes = resBlueprint.getVlanFlow().getEthPipes();
+        Set<ReservedMplsPipeE> mplsPipes = resBlueprint.getVlanFlow().getMplsPipes();
+
+        Set<String> combinedAzERO = new HashSet<>();
+        Set<String> combinedZaERO = new HashSet<>();
+        for(ReservedEthPipeE ethPipe : ethPipes){
+            combinedAzERO.add(ethPipe.getAJunction().getDeviceUrn().getUrn());
+            combinedAzERO.addAll(ethPipe.getAzERO());
+            combinedAzERO.add(ethPipe.getZJunction().getDeviceUrn().getUrn());
+
+            combinedZaERO.add(ethPipe.getZJunction().getDeviceUrn().getUrn());
+            combinedZaERO.addAll(ethPipe.getZaERO());
+            combinedZaERO.add(ethPipe.getAJunction().getDeviceUrn().getUrn());
+        }
+        for(ReservedMplsPipeE mplsPipe : mplsPipes){
+            combinedAzERO.add(mplsPipe.getAJunction().getDeviceUrn().getUrn());
+            combinedAzERO.addAll(mplsPipe.getAzERO());
+            combinedAzERO.add(mplsPipe.getZJunction().getDeviceUrn().getUrn());
+
+            combinedZaERO.add(mplsPipe.getZJunction().getDeviceUrn().getUrn());
+            combinedZaERO.addAll(mplsPipe.getZaERO());
+            combinedZaERO.add(mplsPipe.getAJunction().getDeviceUrn().getUrn());
+        }
+
+        assert(combinedAzERO.stream().noneMatch(blacklist::contains));
+        assert(combinedZaERO.stream().noneMatch(blacklist::contains));
+        log.info("'pceSubmitBlacklistAllTest' passed.");
+    }
+
+    public RequestedBlueprintE setupTest(String srcPort, String srcDevice, String dstPort, String dstDevice, Set<String> blacklist){
+        Date startDate = new Date(Instant.now().plus(15L, ChronoUnit.MINUTES).getEpochSecond());
+        Date endDate = new Date(Instant.now().plus(1L, ChronoUnit.DAYS).getEpochSecond());
         Integer azBW = 25;
         Integer zaBW = 25;
         PalindromicType palindrome = PalindromicType.PALINDROME;
         SurvivabilityType survivability = SurvivabilityType.SURVIVABILITY_NONE;
         String vlan = "any";
-        requestedSched = testBuilder.buildSchedule(startDate, endDate);
-        requestedBlueprint = testBuilder.buildRequest(srcPort, srcDevice, dstPort, dstDevice, azBW, zaBW, palindrome, survivability, vlan, blacklist);
+        return testBuilder.buildRequest(srcPort, srcDevice, dstPort, dstDevice, azBW, zaBW, palindrome, survivability, vlan, blacklist);
+    }
 
-
-        log.info("Beginning test: 'blacklistMplsTest'.");
+    public void pruneTest(RequestedBlueprintE requestedBlueprint, Topology topo, ScheduleSpecificationE requestedSched,
+                        Set<TopoEdge> origEdges, Set<TopoVertex> origVerts, Set<String> blacklist){
         RequestedVlanPipeE reqPipe = requestedBlueprint.getVlanFlow().getPipes().iterator().next();
         topo = pruningService.pruneWithPipe(topo, reqPipe, requestedSched);
         Set<TopoEdge> newEdges = topo.getEdges();
@@ -92,8 +264,6 @@ public class BlacklistPruningTest {
         assert(origEdges.size() == newEdges.size() + removedEdges.size());
         assert(origVerts.size() == newVerts.size());
         assert(newEdges.stream().noneMatch(edge -> blacklist.contains(edge.getA().getUrn()) || blacklist.contains(edge.getZ().getUrn())));
-
-        log.info("'blacklistMplsTest' passed.");
     }
 
 }
