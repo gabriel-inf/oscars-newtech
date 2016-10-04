75e46e005e99ca7affabf12ef514c9075181214b
DylanAPDavis <dylan.ap.davis@gmail.com>
2016-10-04 10:09:40 -0400

    Remove specificationID from VLAN Specification.

--- shared/src/main/java/net/es/oscars/dto/spec/VlanSpecification.java
+++ shared/src/main/java/net/es/oscars/dto/spec/VlanSpecification.java
@@ -10,7 +10,6 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VlanSpecification {
-    private Long specificationId;
 
     @NonNull
     private String connectionId;
