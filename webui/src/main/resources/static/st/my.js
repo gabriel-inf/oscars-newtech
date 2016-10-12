ed24e042ce8f29140d37b09eb2716d3cfb09598f
haniotak <h8XntUEBNae6>
2016-10-12 10:08:47 -0700

    ui progress

--- webui/src/main/resources/static/st/my.js
+++ webui/src/main/resources/static/st/my.js
@@ -3,6 +3,9 @@ var display_viz = {};
 var reservation_viz = {};
 var add_form;
 
+var resv_edge_params_form;
+var resv_node_params_form;
+
 
 function loadJSON(url, callback) {
 
@@ -19,20 +22,45 @@ function loadJSON(url, callback) {
     xobj.send(null);
 }
 
-function add_to_reservation() {
+function add_to_reservation(viz) {
+
+    var last_added_node = null;
+    var ds = viz.datasource;
+    var prev_len = ds.nodes.length;
+    console.log("existing nodes num: " + prev_len);
+
+    if (prev_len > 0) {
+        last_added_node = ds.nodes.get()[prev_len - 1];
+        console.log("last added: " + last_added_node.id);
+    }
 
     for (var i = 0; i < selected_node_ids.length; i++) {
         var nodeId = selected_node_ids[i];
-        console.log("adding node "+nodeId);
-        var nodes = reservation_viz.datasource.nodes;
-        if (!nodes.get(nodeId)) {
-            nodes.add({id: nodeId, label: nodeId});
+        console.log("adding node " + nodeId);
+        if (!ds.nodes.get(nodeId)) {
+            ds.nodes.add({id: nodeId, label: nodeId});
+            if (last_added_node != null) {
+                var a = last_added_node.id;
+                var z = nodeId;
+                var newId = a + " -- " + z;
+                var newEdge = {
+                    id: newId,
+                    from: a,
+                    to: z
+                };
+                console.log("adding edge: " + newId);
 
+                ds.edges.add(newEdge);
+
+            }
         }
     }
+
+    viz.network.stabilize();
+
 }
 
-function make_network(json_data, container, options) {
+function make_network(json_data, container, options, is_resv_form) {
 
     // create an array with nodes
     var nodes = new vis.DataSet(json_data['nodes']);
@@ -50,11 +78,11 @@ function make_network(json_data, container, options) {
     result.network = network;
     result.datasource = datasource;
 
-    attach_handlers(network, datasource);
+    attach_handlers(network, datasource, is_resv_form);
     return result;
 }
 
-function attach_handlers(vis_js_network, vis_js_datasets) {
+function attach_handlers(vis_js_network, vis_js_datasets, is_resv_form) {
 
     vis_js_network.on('dragEnd', function (params) {
         selected_node_ids = [];
@@ -98,6 +126,7 @@ function attach_handlers(vis_js_network, vis_js_datasets) {
 
     vis_js_network.on("click", function (params) {
         var clickedNode = false;
+        var clickedEdge = false;
         var clickedPlain = false;
         selected_node_ids = [];
         var i;
@@ -117,16 +146,33 @@ function attach_handlers(vis_js_network, vis_js_datasets) {
             }
         }
 
+
         if (!clickedNode) {
             for (i = 0; i < params.edges.length; i++) {
+                clickedEdge = true;
                 var edgeId = params.edges[i];
                 edgeId = vis_js_network.clustering.getBaseEdge(edgeId);
                 console.log("edge selected: " + edgeId);
-
             }
         }
+
+        if (is_resv_form) {
+            if (clickedEdge) {
+                resv_edge_params_form.show()
+            } else {
+                resv_edge_params_form.hide();
+            }
+            if (clickedNode) {
+                resv_node_params_form.show()
+            } else {
+                resv_node_params_form.hide();
+            }
+        }
+
         if (clickedPlain) {
             add_form.show();
+        } else {
+            add_form.hide();
         }
     });
 }
@@ -154,7 +200,7 @@ $(document).ready(function () {
                 color: {background: "white"}
             }
         };
-        display_viz = make_network(json_data, nv_cont, nv_opts);
+        display_viz = make_network(json_data, nv_cont, nv_opts, false);
 
         var rv_cont = document.getElementById('reservation_viz');
         var rv_opts = {
@@ -169,17 +215,31 @@ $(document).ready(function () {
             nodes: {
                 shape: 'dot',
                 color: {background: "white"}
+            },
+            manipulation: {
+                addEdge: function (data, callback) {
+                    if (data.from != data.to) {
+                        callback(data);
+                    }
+                }
             }
         };
 
-        reservation_viz = make_network({}, rv_cont, rv_opts);
+        reservation_viz = make_network({}, rv_cont, rv_opts, true);
+
+        resv_edge_params_form = $('#resv_edge_params_form');
+        resv_edge_params_form.hide();
+
+        resv_node_params_form = $('#resv_node_params_form');
+        resv_node_params_form.hide();
+
 
         add_form = $('#add_to_resv_form');
         add_form.hide();
 
         add_form.on('submit', function (e) {
             e.preventDefault();
-            add_to_reservation();
+            add_to_reservation(reservation_viz);
         });
 
 
