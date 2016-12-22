4ca0a5c7117e91ad3a1bdb72b743aca7a086f84d
DylanAPDavis <dylan.ap.davis@gmail.com>
2016-12-22 16:56:21 -0500

    Add basic React NavBar.

--- webui/src/main/js/app.js
+++ webui/src/main/js/app.js
@@ -4,6 +4,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom')
 const client = require('./client');
+const NavBar = require('./navbar');
 // end::vars[]
 
 // tag::app[]
@@ -11,19 +12,21 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {isAuthenticated: true, isAdmin: true};
     }
 
     render() {
         return (
-            <h1>Hello World</h1>
+            <NavBar isAuthenticated={this.state.isAuthenticated} isAdmin={this.state.isAdmin}/>
         );
     }
 }
 // end::app[]
 
+
 // tag::render[]
 ReactDOM.render(
 <App />,
     document.getElementById('react')
-)
+);
 // end::render[]
---
+++ webui/src/main/js/navbar.js
@@ -0,0 +1,85 @@
+const React = require('react');
+
+// tag::navbar[]
+class NavBar extends React.Component{
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">OSCARS</a>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li><a href="/">Reservation List</a></li>
+                            <li><a href="/resv/gui">New Reservation</a></li>
+                            <li><a href="/resv/timebw">What-If?</a></li>
+                            {this.props.isAuthenticated ?
+                                <AuthenticatedNavBarElement /> :
+                                <div></div>
+                            }
+                            {this.props.isAdmin ?
+                                <AdminNavBarElement /> :
+                                <div></div>
+                            }
+                            {this.props.isAuthenticated ?
+                                <li><a href="/logout">Log out</a></li> :
+                                <div></div>
+                            }
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+class AuthenticatedNavBarElement extends React.Component{
+
+    render(){
+        return (
+            <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown"
+                   role="button" aria-haspopup="true" aria-expanded="false">
+                    Reservations<span className="caret"></span></a>
+
+                <ul className="dropdown-menu">
+                    <li><a href="/resv/list">List</a></li>
+                    <li><a href="/resv/gui">New</a></li>
+                </ul>
+            </li>
+        )
+    }
+}
+
+class AdminNavBarElement extends React.Component{
+
+    render(){
+        return (
+            <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown"
+                   role="button" aria-haspopup="true" aria-expanded="false">Administration
+                    <span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                    <li><a href="/admin/user_list">User List</a></li>
+                    <li><a href="/admin/user_add">Add a user</a></li>
+                    <li><a href="/admin/comp_list">Components</a></li>
+                    <li><a href="/admin/group_list">Topology</a></li>
+                    <li><a href="/admin/cust_list">Accounting</a></li>
+                </ul>
+            </li>
+        )
+    }
+}
+// end::navbar[]
+
+module.exports = NavBar;
