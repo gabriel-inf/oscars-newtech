0c9648736ebc8e46ec6f3b86311994abb255be11
haniotak <haniotak@gmail.com>
2017-03-24 11:32:20 -0700

    add lots more topology consistency checks

--- core/src/main/java/net/es/oscars/topo/svc/ConsistencyChecker.java
+++ core/src/main/java/net/es/oscars/topo/svc/ConsistencyChecker.java
@@ -1,14 +1,13 @@
 package net.es.oscars.topo.svc;
 
 import lombok.extern.slf4j.Slf4j;
+import net.es.oscars.dto.topo.enums.Layer;
 import net.es.oscars.dto.topo.enums.UrnType;
 import net.es.oscars.pss.dao.UrnAddressRepository;
 import net.es.oscars.pss.ent.UrnAddressE;
-import net.es.oscars.resv.dao.ReservedBandwidthRepository;
-import net.es.oscars.topo.dao.ReservableBandwidthRepository;
-import net.es.oscars.topo.dao.ReservableVlanRepository;
 import net.es.oscars.topo.dao.UrnAdjcyRepository;
 import net.es.oscars.topo.dao.UrnRepository;
+import net.es.oscars.topo.ent.UrnAdjcyE;
 import net.es.oscars.topo.ent.UrnE;
 import net.es.oscars.ui.pop.UIPopulator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +15,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -51,7 +47,12 @@ public class ConsistencyChecker {
 
         Map<String, Boolean> checks = new HashMap<>();
         checks.put("device addresses", this.checkDeviceAddresses());
-        checks.put("positions", this.checkPositions());
+        checks.put("mpls ifce addresses", this.checkMplsIfceAddresses());
+        checks.put("address urns exist", this.checkAddressUrnsExist());
+        checks.put("inverse adjacencies", this.checkInverseAdjacencies());
+
+        checks.put("positions match devices", this.checkAllPositionsHaveDevices());
+        checks.put("devices have positions", this.checkDevicePositions());
 
         boolean result = true;
         for (String key : checks.keySet()) {
@@ -64,8 +65,8 @@ public class ConsistencyChecker {
         return result;
     }
 
-    public boolean checkPositions() {
-        log.debug("checking positions consistency..");
+    public boolean checkDevicePositions() {
+        log.debug("checking that all devices have positions ..");
         Set<String> devicesInPositions = this.ui.getPositions().keySet();
         Set<String> devicesInUrns = this.urnRepo.findAll().stream()
                 .filter(t -> t.getUrnType().equals(UrnType.DEVICE))
@@ -75,37 +76,119 @@ public class ConsistencyChecker {
         Set<String> devicesWithoutPositions = new HashSet<>();
         for (String device : devicesInUrns) {
             if (!devicesInPositions.contains(device)) {
+                log.error("position entry missing for device "+device);
                 devicesWithoutPositions.add(device);
             }
         }
-        Set<String> positionsWithoutDevices = new HashSet<>();
-        for (String device : devicesInPositions) {
-            if (!devicesInUrns.contains(device)) {
-                positionsWithoutDevices.add(device);
-            }
-        }
-        if (positionsWithoutDevices.isEmpty() && devicesWithoutPositions.isEmpty()) {
-            return true;
-        }
-        return false;
+        return devicesWithoutPositions.isEmpty();
     }
-    public boolean checkDeviceAddresses() {
-        log.debug("checking address consistency..");
+    public boolean checkAllPositionsHaveDevices() {
+        log.debug("checking that all position entries match a device Urn..");
+        Set<String> devicesInPositions = this.ui.getPositions().keySet();
         Set<String> devicesInUrns = this.urnRepo.findAll().stream()
                 .filter(t -> t.getUrnType().equals(UrnType.DEVICE))
                 .map(UrnE::getUrn)
                 .collect(Collectors.toSet());
-        Set<String> urnsWithAddresses = this.urnAddrRepo.findAll().stream()
-                .map(UrnAddressE::getUrn).collect(Collectors.toSet());
+        Set<String> positionsWithoutDevices = new HashSet<>();
 
-        Set<String> devicesWithoutAddrs = new HashSet<>();
-        for (String device : devicesInUrns) {
-            if (!urnsWithAddresses.contains(device)) {
-                devicesWithoutAddrs.add(device);
+        for (String device : devicesInPositions) {
+            if (!devicesInUrns.contains(device)) {
+                positionsWithoutDevices.add(device);
+                log.error("urn in position entry not found: "+device);
             }
         }
+        return positionsWithoutDevices.isEmpty();
 
-        return devicesWithoutAddrs.isEmpty();
+    }
+
+    public boolean checkDeviceAddresses() {
+        log.debug("checking device addresses ..");
+        Set<String> urnsThatShouldHaveAddresses = this.urnRepo.findAll().stream()
+                .filter(t -> t.getUrnType().equals(UrnType.DEVICE))
+                .map(UrnE::getUrn)
+                .collect(Collectors.toSet());
+        return this.checkIfAddressesExist(urnsThatShouldHaveAddresses);
+    }
+
+    public boolean checkAddressUrnsExist() {
+        log.debug("checking if address URNs all exist..");
+        Set<String> allUrnsWithAddresses = this.urnAddrRepo.findAll().stream()
+                .map(UrnAddressE::getUrn).collect(Collectors.toSet());
+        Set<String> allKnownUrns = this.urnRepo.findAll().stream()
+                .map(UrnE::getUrn).collect(Collectors.toSet());
+
+        boolean result = true;
+        for (String urn : allUrnsWithAddresses) {
+            if (!allKnownUrns.contains(urn)) {
+                log.error("have address entry for non-existent "+urn);
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public boolean checkMplsIfceAddresses() {
+        log.debug("checking mpls ifce addresses..");
+        Set<String> urnsThatShouldHaveAddresses = this.urnRepo.findAll().stream()
+                .filter(t -> t.getUrnType().equals(UrnType.IFCE) && t.getCapabilities().contains(Layer.MPLS))
+                .map(UrnE::getUrn)
+                .collect(Collectors.toSet());
+        return this.checkIfAddressesExist(urnsThatShouldHaveAddresses);
+    }
+    private boolean checkIfAddressesExist(Set<String> urnsThatShouldHaveAddresses) {
+
+        Set<String> allUrnsWithAddresses = this.urnAddrRepo.findAll().stream()
+                .map(UrnAddressE::getUrn).collect(Collectors.toSet());
+
+        Set<String> urnsThatShouldHaveAddressesButDoNot = new HashSet<>();
+        for (String urn : urnsThatShouldHaveAddresses) {
+            if (!allUrnsWithAddresses.contains(urn)) {
+                log.error("ip address missing for urn "+urn);
+                urnsThatShouldHaveAddressesButDoNot.add(urn);
+            }
+        }
+        return urnsThatShouldHaveAddressesButDoNot.isEmpty();
+    }
+
+
+    public boolean checkInverseAdjacencies() {
+        log.debug("checking inverse adjacencies..");
+        List<UrnAdjcyE> adjcies = this.adjcyRepo.findAll();
+        boolean result = true;
+        for (UrnAdjcyE adjcy : adjcies) {
+            UrnE a = adjcy.getA();
+            UrnE z = adjcy.getZ();
+            Map<Layer, Long> metrics = adjcy.getMetrics();
+            boolean inverseFound = false;
+            boolean inverseMetricsOk = false;
+            for (UrnAdjcyE maybeInverse : adjcies) {
+                if (maybeInverse.getZ().equals(a) && maybeInverse.getA().equals(z)) {
+                    inverseFound = true;
+                    inverseMetricsOk = true;
+
+                    boolean allMetricsFound = true;
+                    for (Layer l : metrics.keySet()) {
+                        if (!maybeInverse.getMetrics().keySet().contains(l)) {
+                            allMetricsFound = false;
+                        }
+                    }
+                    if (!allMetricsFound) {
+                        inverseMetricsOk = false;
+                    }
+                }
+            }
+
+            if (!inverseFound) {
+                log.error("could not find inverse for "+a.getUrn()+" -- "+z.getUrn());
+                result = false;
+            } else if (!inverseMetricsOk) {
+                log.error("could not find matching metrics for "+a.getUrn()+" -- "+z.getUrn());
+                result = false;
+            }
+        }
+        return result;
+
+
 
     }
 
--- core/src/test/java/net/es/oscars/consistency/ConsistencyCheckingTest.java
+++ core/src/test/java/net/es/oscars/consistency/ConsistencyCheckingTest.java
@@ -5,14 +5,17 @@ import net.es.oscars.AbstractCoreTest;
 import net.es.oscars.QuickTests;
 import net.es.oscars.dto.topo.enums.DeviceModel;
 import net.es.oscars.dto.topo.enums.DeviceType;
+import net.es.oscars.dto.topo.enums.Layer;
 import net.es.oscars.dto.topo.enums.UrnType;
 import net.es.oscars.dto.viz.Position;
-import net.es.oscars.pce.helpers.RepoEntityBuilder;
 import net.es.oscars.pss.dao.UrnAddressRepository;
 import net.es.oscars.pss.ent.UrnAddressE;
+import net.es.oscars.pss.pop.UrnAddressImporter;
 import net.es.oscars.topo.dao.UrnAdjcyRepository;
 import net.es.oscars.topo.dao.UrnRepository;
+import net.es.oscars.topo.ent.UrnAdjcyE;
 import net.es.oscars.topo.ent.UrnE;
+import net.es.oscars.topo.pop.TopoFileImporter;
 import net.es.oscars.topo.prop.TopoProperties;
 import net.es.oscars.topo.svc.ConsistencyChecker;
 import net.es.oscars.ui.pop.UIPopulator;
@@ -21,8 +24,11 @@ import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 
 @Slf4j
@@ -47,11 +53,42 @@ public class ConsistencyCheckingTest extends AbstractCoreTest {
         urnRepo.deleteAll();
         adjcyRepo.deleteAll();
         urnAddrRepo.deleteAll();
+        adjcyRepo.deleteAll();
+
     }
 
     @Test
     @Category(QuickTests.class)
-    public void testConsistencyOk() {
+    public void checkAllTopologies() throws IOException {
+
+        List<String> prefixes = new ArrayList<>();
+        prefixes.add("esnet");
+        prefixes.add("netlab");
+        prefixes.add("testbed");
+        for (String prefix : prefixes) {
+            this.clear();
+
+            TopoProperties topoProperties = new TopoProperties();
+            topoProperties.setPrefix(prefix);
+
+            ui = new UIPopulator(topoProperties);
+            UrnAddressImporter uai = new UrnAddressImporter(topoProperties, urnAddrRepo);
+            TopoFileImporter tfi = new TopoFileImporter(urnRepo, adjcyRepo, topoProperties);
+
+            ui.startup();
+            uai.startup();
+            tfi.startup();
+
+            ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
+            assert consistencyChecker.checkConsistency();
+            log.info("topology "+prefix+ " is consistent");
+        }
+
+    }
+
+    @Test
+    @Category(QuickTests.class)
+    public void testEverythingOk() {
         this.clear();
         Position p = Position.builder().x(1).y(1).build();
         ui.getPositions().put("foo-cr1", p);
@@ -72,6 +109,37 @@ public class ConsistencyCheckingTest extends AbstractCoreTest {
                 .build();
         urnAddrRepo.save(addr);
 
+        UrnE a = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:a")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        UrnE z = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:z")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        urnRepo.save(a);
+        urnRepo.save(z);
+
+        UrnAdjcyE az = UrnAdjcyE.builder()
+                .a(a)
+                .z(z)
+                .metrics(new HashMap<>())
+                .build();
+        az.getMetrics().put(Layer.ETHERNET, 100L);
+        UrnAdjcyE za = UrnAdjcyE.builder()
+                .a(z)
+                .z(a)
+                .metrics(new HashMap<>())
+                .build();
+        za.getMetrics().put(Layer.ETHERNET, 100L);
+        adjcyRepo.save(az);
+        adjcyRepo.save(za);
+
+
         ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
 
         assert consistencyChecker.checkConsistency();
@@ -79,11 +147,98 @@ public class ConsistencyCheckingTest extends AbstractCoreTest {
 
     @Test
     @Category(QuickTests.class)
-    public void testBadPositions() {
+    public void testNoInverseAdjcies() {
         this.clear();
 
-        Position p = Position.builder().x(1).y(1).build();
-        ui.getPositions().put("foo-cr2", p);
+        UrnE a = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:a")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        UrnE b = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:b")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        UrnE z = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:z")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        urnRepo.save(a);
+        urnRepo.save(b);
+        urnRepo.save(z);
+
+        UrnAdjcyE ab = UrnAdjcyE.builder()
+                .a(a)
+                .z(b)
+                .metrics(new HashMap<>())
+                .build();
+        ab.getMetrics().put(Layer.ETHERNET, 100L);
+        UrnAdjcyE za = UrnAdjcyE.builder()
+                .a(z)
+                .z(a)
+                .metrics(new HashMap<>())
+                .build();
+        za.getMetrics().put(Layer.ETHERNET, 100L);
+        adjcyRepo.save(ab);
+        adjcyRepo.save(za);
+
+
+        ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
+        assert !consistencyChecker.checkConsistency();
+
+    }
+
+
+    @Test
+    @Category(QuickTests.class)
+    public void testAdcjyLayerMismatch() {
+        this.clear();
+        UrnE a = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:a")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        UrnE z = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:z")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        urnRepo.save(a);
+        urnRepo.save(z);
+
+        UrnAdjcyE az = UrnAdjcyE.builder()
+                .a(a)
+                .z(z)
+                .metrics(new HashMap<>())
+                .build();
+        az.getMetrics().put(Layer.INTERNAL, 100L);
+        az.getMetrics().put(Layer.ETHERNET, 100L);
+        UrnAdjcyE za = UrnAdjcyE.builder()
+                .a(z)
+                .z(a)
+                .metrics(new HashMap<>())
+                .build();
+        za.getMetrics().put(Layer.ETHERNET, 100L);
+        adjcyRepo.save(az);
+        adjcyRepo.save(za);
+
+
+        ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
+        assert !consistencyChecker.checkConsistency();
+
+    }
+
+    @Test
+    @Category(QuickTests.class)
+    public void testDeviceWithoutPosition() {
+        this.clear();
 
         UrnE foo_cr1 = UrnE.builder()
                 .capabilities(new HashSet<>())
@@ -109,11 +264,22 @@ public class ConsistencyCheckingTest extends AbstractCoreTest {
 
     @Test
     @Category(QuickTests.class)
-    public void testBadAddress() {
+    public void testPositionWithoutDevice() {
         this.clear();
 
         Position p = Position.builder().x(1).y(1).build();
         ui.getPositions().put("foo-cr1", p);
+
+        ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
+
+        assert !consistencyChecker.checkConsistency();
+    }
+
+
+    @Test
+    @Category(QuickTests.class)
+    public void testNoDeviceAddress() {
+        this.clear();
 
         UrnE foo_cr1 = UrnE.builder()
                 .capabilities(new HashSet<>())
@@ -124,13 +290,41 @@ public class ConsistencyCheckingTest extends AbstractCoreTest {
                 .valid(true)
                 .build();
         urnRepo.save(foo_cr1);
+        ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
+        assert !consistencyChecker.checkConsistency();
+    }
+
+    @Test
+    @Category(QuickTests.class)
+    public void testAddressUrnNotExist() {
+        this.clear();
 
         UrnAddressE addr = UrnAddressE.builder()
                 .ipv4Address("10.1.1.1")
                 .ipv6Address("")
-                .urn("foo-cr2")
+                .urn("foo-cr1")
                 .build();
         urnAddrRepo.save(addr);
+
+
+        ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
+        assert !consistencyChecker.checkConsistency();
+    }
+
+
+    @Test
+    @Category(QuickTests.class)
+    public void testNoMplsIfceAddress() {
+        this.clear();
+
+        UrnE foo = UrnE.builder()
+                .capabilities(new HashSet<>())
+                .urn("foo-cr1:a")
+                .urnType(UrnType.IFCE)
+                .valid(true)
+                .build();
+        foo.getCapabilities().add(Layer.MPLS);
+        urnRepo.save(foo);
 
         ConsistencyChecker consistencyChecker = new ConsistencyChecker(adjcyRepo, urnRepo, urnAddrRepo, ui);
 
